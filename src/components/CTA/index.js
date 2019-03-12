1673e3e5e7eb51d63a787fdab16ad7730b58a2a4
Brock Stone <brockstonedesign@gmail.com>
2019-03-11 22:10:05 -0400

    added new CTA bg

--- src/components/CTA/index.js
+++ src/components/CTA/index.js
@@ -11,12 +11,9 @@ class CTA extends Component {
 		  	<div className='welcome-desc'>
 		  		
 		  		<h1>Rhylee<br/>&amp;<br/>Brock</h1>
-		  			
-		  		
+		  		<p>We're getting married... Again!</p>
 
-		  		<p>Rhylee and Brock are getting married... Again!</p>
-
-		  		<button className='button' onClick={()=>console.log('CLICKED')}>read our stroy</button>
+		  		<button className='button' onClick={()=>console.log('CLICKED')}>read our story</button>
 		  	</div>
 		  </div>
 		   <div class="column sidebar">
